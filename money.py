b9615c481b000e2ed32f6d3dd2ad16921e89131b
Bettina Johnson <johnsonbettina17@gmail.com>
2020-06-29 14:21:48 +0000

    confused but pushing

--- money.py
+++ money.py
@@ -18,47 +18,64 @@ class Currency:
         - symbol - optional symbol used to designate currency
         - digits -- number of significant digits used
         """
-        pass
+
+        self.name = name
+        self.code = code
+        self.symbol = symbol
+        self.digits = digits 
 
     def __str__(self):
         """
         Should return the currency code, or code with symbol in parentheses.
         """
-        pass
+        return f"{self.name} of {self.digits}"
+        return f"{self.symbol} of {self.code}"
 
     def __eq__(self, other):
         """
         All fields must be equal to for the objects to be equal.
         """
-        return (type(self) == type(other) and self.name == other.name and
-                self.code == other.code and self.symbol == other.symbol and
-                self.digits == other.digits)
 
+        return (type(self) == type(other) and self.name == other.name and self.code == other.code and self.symbol == other.symbol and self.digits == other.digits)
 
 class Money:
     """
     Represents an amount of money. Requires an amount and a currency.
     """
+def __init__(self, amount = '0', currency = ''):
+  self.amount = amount
+  self.currency = currency
 
-    def __init__(self, amount, currency):
+
+# def __str__(self):
+#           return f"{self.amount} {self.currency}"
+
+# for amount in amount:
+#   for currency in currencies:
+
+
+
+def __init__(self, amount, currency):
         """
         Parameters:
         - amount -- quantity of currency
         - currency -- type of currency
         """
-        pass
+        self.amount = amount
+        self.currency = currency
+      
 
-    def __str__(self):
+def __str__(self):
         """
         Should use the currency symbol if available, else use the code.
         Use the currency digits to determine number of digits to show.
         """
-        pass
+  pass
 
-    def __repr__(self):
+  def __repr__(self):
         return f"<Money {str(self)}>"
 
-    def __eq__(self, other):
+  def __eq__(self, other):
         """
         All fields must be equal to for the objects to be equal.
         """
@@ -66,28 +83,34 @@ class Money:
         return (type(self) == type(other) and self.amount == other.amount and
                 self.currency == other.currency)
 
-    def add(self, other):
+  def add(self, other):
         """
         Add two money objects of the same currency. If they have different
         currencies, raise a DifferentCurrencyError.
         """
         pass
 
-    def sub(self, other):
+  def sub(self, other):
         """
         Subtract two money objects of the same currency. If they have different
         currencies, raise a DifferentCurrencyError.
         """
         pass
 
-    def mul(self, multiplier):
+  def mul(self, multiplier):
         """
         Multiply a money object by a number to get a new money object.
         """
         pass
 
-    def div(self, divisor):
+  def div(self, divisor):
         """
         Divide a money object by a number to get a new money object.
         """
         pass
+
+
+
+
+
+ 
